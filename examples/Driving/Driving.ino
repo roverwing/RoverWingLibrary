080b3e0d018f94a3a25c7aa1e2e36c808f603355
david115599 <david.bershadsky2@gmail.com>
2019-05-12 22:53:22 -0400

    added imu calibration to code

--- examples/Driving/Driving.ino
+++ examples/Driving/Driving.ino
@@ -35,6 +35,7 @@ void setup(){
   Wire.begin();
   //Wire.setClock(400000); //use fast mode (400 kHz)
   Serial.begin(9600); //debugging terminal
+  r.IMUcalibrate(accelOffset, gyroOffset);
   delay(1000); //wait for 1 second, so that roverwing initializes
   //activates RoverWing and prints basic info to Serial
   r.beginVerbose();
